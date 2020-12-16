c8e687bc1edd14a44d04a6e6cd17d77ff0e079b7
Eduardo Valenzuela <e.valenzuelap@alumnos.duoc.cl>
2020-12-16 02:23:22 -0300

    🎉 Commit initial

---
+++ components/Animacion2.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, Animated } from 'react-native';
+
+const Animacion2 = () => {
+
+    const [animacion] = useState( new Animated.Value(0));
+
+    useEffect(() => {
+        Animated.timing(
+            animacion, {
+                toValue: 450, //al valor que llega
+                duration:500 //cantidad de tiempo en llegar
+            }
+        ).start(); //inicia la animacion
+    }, []);
+
+    return (
+        <Animated.View style={
+            [styles.caja, {
+                width:animacion,
+                height:animacion
+            }]
+        }>
+        </Animated.View>
+    )
+}
+
+const styles = StyleSheet.create({
+    caja:{
+        width: 100,
+        height: 100,
+        backgroundColor: 'cornflowerblue'
+    }
+})
+
+export default Animacion2
